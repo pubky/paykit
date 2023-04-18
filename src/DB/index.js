c7ca740de8080fde7f8a748ac598e43a4a523721
dzdidi <deniszalessky@gmail.com>
2023-04-18 20:58:03 +0200

    mock objects for DB and Slashtags DAO
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ src/DB/index.js
@@ -0,0 +1,53 @@
+const ERROR = {
+  NOT_READY: 'DB is not ready',
+}
+class DB {
+  constructor() {
+    this.db = {}
+    this.ready = false
+  }
+
+  async init() {
+    this.ready = true
+  }
+
+  async save(payment) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    this.db[payment.id] = {
+      ...payment,
+      removed: false,
+    }
+  }
+
+  async update(id, update) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    this.db[id] = {
+      ...this.db[id],
+      ...update,
+    }
+  }
+
+  async delete(id) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    delete this.db[id]
+  }
+
+  async get(id, options = { removed: false }) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    const res = this.db[id]
+    if (!res) return null
+
+    const copy = { ...res }
+    delete copy.removed
+
+    if (options.removed === '*') return copy
+    if (options.removed === true) return res.removed === true ? copy : null
+    if (options.removed === false) return res.removed === false ? copy : null
+  }
+}
+
+module.exports = { DB, ERROR }
---
+++ src/SlashtagsAccessObject/index.js
@@ -0,0 +1,46 @@
+const ERROR = {
+  NOT_READY: 'SlashtagsAccessObject is not ready',
+}
+
+class SlashtagsAccessObject {
+  constructor (key, directory) {
+    this.key = key
+    this.directory = directory
+    this.ready = false
+
+    this.data = {}
+  }
+
+  async init () {
+    this.ready = true
+  }
+
+  async read (key) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    return this.data[key]
+  }
+
+  async create (key, value) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    this.data[key] = value
+  }
+
+  async delete (key) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    delete this.data[key]
+  }
+
+  async update (key, value) {
+    if (!this.ready) throw new Error(ERROR.NOT_READY)
+
+    this.data[key] = value
+  }
+}
+
+module.exports = {
+  SlashtagsAccessObject,
+  ERROR,
+}
