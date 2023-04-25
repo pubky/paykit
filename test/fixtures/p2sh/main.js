172cd24711085f9b52fa0e4c6c41efaf0330c5cb
dzdidi <deniszalessky@gmail.com>
2023-04-25 07:40:56 +0200

    payment utils
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/fixtures/p2sh/main.js
+++ test/fixtures/p2sh/main.js
@@ -1,14 +1,21 @@
 const sinon = require('sinon')
 
+const instance = {
+  stop: sinon.fake(() => {}),
+  onEvent: sinon.fake(() => {}),
+  pay: sinon.fake(() => {}),
+  updatePayment: sinon.fake(() => {})
+}
+
+function resetAll() {
+  instance.stop.resetHistory()
+  instance.onEvent.resetHistory()
+  instance.pay.resetHistory()
+  instance.updatePayment.resetHistory()
+}
+
 module.exports = {
-  init: sinon.fake(() => {
-    return {
-      stop: sinon.fake(() => {}),
-      onEvent: sinon.fake(() => {}),
-      pay: sinon.fake(() => {}),
-      updatePayment: sinon.fake(() => {})
-    }
-  }),
+  init: sinon.fake(() => instance),
   getmanifest: sinon.fake(() => {
     return {
       name: 'p2sh',
@@ -17,5 +24,7 @@ module.exports = {
       rpc: ['stop', 'pay', 'updatePayment'],
       events: ['watch', 'event1', 'event2']
     }
-  })
+  }),
+  instance,
+  resetAll
 }
