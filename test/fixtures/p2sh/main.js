0a913a1e6a527f07b531527c603e648d511d68a1
dzdidi <deniszalessky@gmail.com>
2023-04-25 09:02:24 +0200

    Fixtures: reset history methods
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/fixtures/p2sh/main.js
+++ test/fixtures/p2sh/main.js
@@ -1,21 +1,43 @@
 const sinon = require('sinon')
 
+const stopFake = sinon.fake(() => {})
+const onEventFake = sinon.fake(() => {})
+const payFake = sinon.fake(() => {})
+const updatePaymentFake = sinon.fake(() => {})
+
+const instance = {
+  stop: stopFake,
+  onEvent: onEventFake,
+  pay: payFake,
+  updatePayment: updatePaymentFake
+}
+
+const initFake = sinon.fake(() => instance)
+const getManifestFake = sinon.fake(() => {
+  return {
+    name: 'p2sh',
+    version: '1.0.0',
+    type: 'payment',
+    rpc: ['stop', 'pay', 'updatePayment'],
+    events: ['watch', 'event1', 'event2']
+  }
+})
+
+const resetAll = () => {
+  initFake.resetHistory()
+  getManifestFake.resetHistory()
+
+  stopFake.resetHistory()
+  onEventFake.resetHistory()
+  payFake.resetHistory()
+  updatePaymentFake.resetHistory()
+}
+
+instance.resetAll = resetAll
+
 module.exports = {
-  init: sinon.fake(() => {
-    return {
-      stop: sinon.fake(() => {}),
-      onEvent: sinon.fake(() => {}),
-      pay: sinon.fake(() => {}),
-      updatePayment: sinon.fake(() => {})
-    }
-  }),
-  getmanifest: sinon.fake(() => {
-    return {
-      name: 'p2sh',
-      version: '1.0.0',
-      type: 'payment',
-      rpc: ['stop', 'pay', 'updatePayment'],
-      events: ['watch', 'event1', 'event2']
-    }
-  })
+  init: initFake,
+  getmanifest: getManifestFake,
+  instance,
+  resetAll
 }
--- test/fixtures/p2tr/main.js
+++ test/fixtures/p2tr/main.js
@@ -1,21 +1,43 @@
 const sinon = require('sinon')
 
+const stopFake = sinon.fake(() => {})
+const startFake = sinon.fake(() => {})
+const onEventFake = sinon.fake(() => {})
+const payFake = sinon.fake(() => {})
+
+const instance = {
+  stop: stopFake,
+  start: startFake,
+  onEvent: onEventFake,
+  pay: payFake
+}
+
+const initFake = sinon.fake(() => instance)
+const getManifestFake = sinon.fake(() => {
+  return {
+    name: 'p2tr',
+    version: '1.0.0',
+    type: 'payment',
+    rpc: ['start', 'stop', 'pay'],
+    events: ['watch', 'event1']
+  }
+})
+
+const resetAll = () => {
+  initFake.resetHistory()
+  getManifestFake.resetHistory()
+
+  startFake.resetHistory()
+  stopFake.resetHistory()
+  onEventFake.resetHistory()
+  payFake.resetHistory()
+}
+
+instance.resetAll = resetAll
+
 module.exports = {
-  init: sinon.fake(() => {
-    return {
-      stop: sinon.fake(() => {}),
-      start: sinon.fake(() => {}),
-      onEvent: sinon.fake(() => {}),
-      pay: sinon.fake(() => {})
-    }
-  }),
-  getmanifest: sinon.fake(() => {
-    return {
-      name: 'p2tr',
-      version: '1.0.0',
-      type: 'payment',
-      rpc: ['start', 'stop', 'pay'],
-      events: ['watch', 'event1']
-    }
-  })
+  init: initFake,
+  getmanifest: getManifestFake,
+  instance,
+  resetAll
 }
