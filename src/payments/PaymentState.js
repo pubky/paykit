5877f3d6fbfc2aaa0498adee4d67140992a70835
dzdidi <deniszalessky@gmail.com>
2023-05-09 16:51:19 +0200

    PaymentState
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ src/payments/PaymentState.js
@@ -0,0 +1,328 @@
+const { isEmptyObject } = require('../utils')
+const logger = require('slashtags-logger')('Slashpay', 'payment-state')
+/**
+ * PaymentState class
+ * @class PaymentState
+ * @property {string} internalState - internal state
+ * @property {string[]} pendingPlugins - pending plugins
+ * @property {string[]} triedPlugins - tried plugins
+ * @property {StatePlugin} currentPlugin - current plugin
+ * @property {StatePlugin} sentByPlugin - sent by plugin
+ * @property {StatePlugin} payment - payment
+ */
+class PaymentState {
+  /**
+   * @param {Payment} payment - payment
+   * @param {string} [payment.internalState] - internal state
+   * @param {string[]} [payment.pendingPlugins] - pending plugins
+   * @param {string[]} [payment.triedPlugins] - tried plugins
+   * @param {StatePlugin} [payment.currentPlugin] - current plugin
+   * @param {StatePlugin} [payment.sentByPlugin] - sent by plugin
+   * @param {Payment} [payment.payment] - payment
+   * @throws {Error} - if payment is not provided
+   */
+  constructor (payment, params = {}) {
+    logger.debug(`Initializing payment state with ${payment}, ${JSON.stringify(params)}`)
+    PaymentState.validate(payment)
+
+    this.internalState = payment.internalState || params.internalState || PAYMENT_STATE.INITIAL
+    this.pendingPlugins = payment.pendingPlugins || params.pendingPlugins || []
+    this.triedPlugins = payment.triedPlugins || params.triedPlugins || []
+
+    this.currentPlugin = payment.currentPlugin || params.currentPlugin || null
+    this.sentByPlugin = payment.sentByPlugin || params.sentByPlugin || null
+
+    this.payment = payment
+    logger.debug('Initialized payment state')
+  }
+
+  /**
+   * Returns string representation of payment state used for logging
+   */
+  [Symbol.for('nodejs.util.inspect.custom')] () {
+    return JSON.stringify(this.serialize())
+  }
+
+  /**
+   * Instance logger for payment state at debug level
+   */
+  debug (msg) { logger.debug.extend(JSON.stringify(this.serialize()))({ msg }) }
+
+  /**
+   * Instance logger for payment state at info level
+   */
+  info (msg) { logger.info.extend(JSON.stringify(this.serialize()))({ msg }) }
+
+  /**
+   * Assigns pending plugins
+   * @param {string[]} pendingPlugins - pending plugins
+   * @throws {Error} - if pendingPlugins is not an array
+   * @returns {void}
+   */
+  assignPendingPlugins (pendingPlugins) {
+    this.debug(`Assigning pending plugins ${pendingPlugins}`)
+
+    if (!Array.isArray(pendingPlugins)) throw new Error(ERRORS.PENDING_PLUGINS_NOT_ARRAY)
+    this.pendingPlugins = [...pendingPlugins]
+
+    this.debug('Assigned pending plugins')
+  }
+
+  /**
+   * Validates payment
+   * @param {Payment} payment - payment
+   * @throws {Error} - if payment is not provided
+   * @throws {Error} - if payment db is not provided
+   * @throws {Error} - if payment db is not ready
+   * @returns {void}
+   */
+  static validate (payment) {
+    logger.debug(`Validating payment ${payment}`)
+
+    if (!payment) throw new Error('Payment is required')
+    if (!payment.db) throw new Error('Payment db is required')
+    if (!payment.db.ready) throw new Error('Payment db is not ready')
+
+    logger.debug('Validated payment')
+  }
+
+  /**
+   * Serializes payment state
+   * @returns {Object} - serialized payment state
+   * @returns {string} [returns.internalState] - internal state
+   * @returns {string[]} [returns.pendingPlugins] - pending plugins
+   * @returns {string[]} [returns.triedPlugins] - tried plugins
+   * @returns {StatePlugin} [returns.currentPlugin] - current plugin
+   * @returns {StatePlugin} [returns.sentByPlugin] - sent by plugin
+   */
+  serialize () {
+    return {
+      internalState: this.internalState,
+      pendingPlugins: [...this.pendingPlugins],
+      triedPlugins: [...this.triedPlugins],
+      currentPlugin: { ...this.currentPlugin },
+      sentByPlugin: { ...this.sentByPlugin }
+    }
+  }
+
+  /**
+   * Returns current state
+   * @returns {string} - current state
+   */
+  currentState = () => this.internalState
+
+  /**
+   * Returns true if current state is initial
+   * @returns {boolean} - true if current state is initial
+   * @returns {boolean} - false if current state is not initial
+   */
+  isInitial = () => this.currentState() === PAYMENT_STATE.INITIAL
+
+  /**
+   * Returns true if current state is in progress
+   * @returns {boolean} - true if current state is in progress
+   * @returns {boolean} - false if current state is not in progress
+   */
+  isInProgress = () => this.currentState() === PAYMENT_STATE.IN_PROGRESS
+
+  /**
+   * Returns true if current state is completed
+   * @returns {boolean} - true if current state is completed
+   * @returns {boolean} - false if current state is not completed
+   */
+  isCompleted = () => this.currentState() === PAYMENT_STATE.COMPLETED
+
+  /**
+   * Returns true if current state is failed
+   * @returns {boolean} - true if current state is failed
+   * @returns {boolean} - false if current state is not failed
+   */
+  isFailed = () => this.currentState() === PAYMENT_STATE.FAILED
+
+  /**
+   * Returns true if current state is cancelled
+   * @returns {boolean} - true if current state is cancelled
+   * @returns {boolean} - false if current state is not cancelled
+   */
+  isCancelled = () => this.currentState() === PAYMENT_STATE.CANCELLED
+
+  /**
+   * Returns true if current state is final
+   * @returns {boolean} - true if current state is completed or failed or cancelled
+   * @returns {boolean} - false if current state is not completed or failed or cancelled
+   */
+  isFinal = () => this.isCompleted() || this.isFailed() || this.isCancelled()
+
+  /**
+   * Cancel payment - sets internal state to cancelled and updates payment in db
+   * @throws {Error} - if current state is not initial
+   */
+  async cancel () {
+    this.info('Cancelling payment')
+    if (!this.isInitial()) throw new Error(ERRORS.INVALID_STATE(this.internalState))
+    if (!isEmptyObject(this.currentPlugin)) {
+      // Belt and suspenders
+      // should not be possible as currentPlugin must not be assigned in initial internalState
+      throw new Error('Cannot cancel while processing')
+    }
+
+    this.internalState = PAYMENT_STATE.CANCELLED
+    await this.payment.update()
+    this.debug('Cancelled payment')
+  }
+
+  /**
+   * Process payment - sets internal state to in progress and updates payment in db for new payments
+   * fails payment if there are no pending plugins and updates payment in db
+   * tries next plugin if there are pending plugins and updates payment in db
+   * @throws {Error} - if current state is not initial
+   * @returns {boolean} - true if next plugin is tried
+   * @returns {boolean} - false if payment is failed
+   */
+  async process () {
+    this.info('Processing payment')
+    if (this.isInitial()) {
+      this.internalState = PAYMENT_STATE.IN_PROGRESS
+      await this.payment.update()
+    }
+
+    if (!isEmptyObject(this.currentPlugin)) throw new Error(ERRORS.PLUGIN_IN_PROGRESS(this.currentPlugin.name))
+
+    if (this.pendingPlugins.length === 0) {
+      await this.fail()
+      return false
+    }
+
+    return await this.tryNext()
+  }
+
+  /**
+   * Mark current plugin as tried with failed state
+   * @returns {void}
+   */
+  async failCurrentPlugin () {
+    this.info('Failing current plugin')
+    if (!this.isInProgress()) throw new Error(ERRORS.INVALID_STATE(this.internalState))
+    // XXX: this should not be possible
+    if (isEmptyObject(this.currentPlugin)) throw new Error('No current plugin')
+
+    this.markCurrentPluginAsTried(PLUGIN_STATE.FAILED)
+    this.debug('Marked current plugin as tried with failed state')
+    await this.payment.update()
+  }
+
+  /**
+   * Fail payment - sets internal state to failed and updates payment in db
+   * @throws {Error} - if current state is not in progress
+   * @returns {void}
+   */
+  async fail () {
+    this.info('Failing payment')
+    if (!this.isInProgress()) throw new Error(ERRORS.INVALID_STATE(this.internalState))
+
+    if (!isEmptyObject(this.currentPlugin)) await this.failCurrentPlugin()
+
+    this.internalState = PAYMENT_STATE.FAILED
+    this.debug('Failed payment')
+    await this.payment.update()
+  }
+
+  /**
+   * Try next plugin - sets current plugin to next pending plugin and updates payment in db
+   * @throws {Error} - if current state is not in progress
+   * @returns {void}
+   */
+  async tryNext () {
+    this.info('Trying next plugin')
+    if (!this.isInProgress()) throw new Error(ERRORS.INVALID_STATE(this.internalState))
+    if (!isEmptyObject(this.currentPlugin)) throw new Error(ERRORS.PLUGIN_IN_PROGRESS(this.currentPlugin.name))
+
+    this.currentPlugin = { name: this.pendingPlugins.shift(), startAt: Date.now(), state: PLUGIN_STATE.SUBMITTED }
+    await this.payment.update()
+    this.debug('Updated payment with next plugin')
+  }
+
+  /**
+   * Complete payment - sets internal state to completed and updates payment in db
+   * @throws {Error} - if current state is not in progress
+   * @returns {void}
+   */
+  async complete () {
+    this.info('Completing payment')
+    if (!this.isInProgress()) throw new Error(ERRORS.INVALID_STATE(this.internalState))
+
+    this.sentByPlugin = this.markCurrentPluginAsTried(PLUGIN_STATE.SUCCESS)
+    this.debug('Marked current plugin as tried with success state')
+    this.internalState = PAYMENT_STATE.COMPLETED
+    await this.payment.update()
+    this.debug('Completed payment')
+  }
+
+  /**
+   * Marks current plugin as tried and returns it
+   * @returns {StatePlugin} - completed current plugin with endAt timestamp
+   */
+  markCurrentPluginAsTried (state) {
+    this.info('Marking current plugin as tried')
+    const completedPlugin = { ...this.currentPlugin, endAt: Date.now(), state }
+    this.triedPlugins.push(completedPlugin)
+    this.currentPlugin = null
+
+    this.debug('Marked current plugin as tried')
+    return completedPlugin
+  }
+}
+
+/**
+ * @typedef {Object} PaymentState
+ * @property {string} INITIAL - initial state
+ * @property {string} IN_PROGRESS - in progress state
+ * @property {string} COMPLETED - completed state
+ * @property {string} FAILED - failed state
+ * @property {string} CANCELLED - cancelled state
+ */
+const PAYMENT_STATE = {
+  INITIAL: 'initial',
+  IN_PROGRESS: 'in_progress',
+  COMPLETED: 'completed',
+  FAILED: 'failed',
+  CANCELLED: 'cancelled'
+}
+
+/**
+ * @typedef {Object} PluginState
+ * @property {string} SUBMITTED - submitted to plugin
+ * @property {string} FAILED - failed by plugin
+ * @property {string} SUCCESS - succeeded by plugin
+ */
+const PLUGIN_STATE = {
+  SUBMITTED: 'submitted',
+  FAILED: 'failed',
+  SUCCESS: 'success'
+}
+
+/**
+ * @typedef {Object} ERRORS
+ * @property {function} INVALID_STATE - returns error message with invalid state
+ * @property {string} PENDING_PLUGINS_NOT_ARRAY - error message for pending plugins not array
+ */
+const ERRORS = {
+  INVALID_STATE: (s) => `Invalid state: ${s}`,
+  PENDING_PLUGINS_NOT_ARRAY: 'Pending plugins must be an array',
+  PLUGIN_IN_PROGRESS: (name) => `Cannot try next plugin while processing ${name}`
+}
+
+/**
+ * typedef {StatePlugin}
+ * @property {string} name - name of the plugin
+ * @property {number} startAt - start time
+ * @property {string} state - state of the plugin
+ * @property {number} [endAt] - end time
+ */
+
+module.exports = {
+  PaymentState,
+  PAYMENT_STATE,
+  PLUGIN_STATE,
+  ERRORS
+}
---
+++ test/payments/PaymentState.test.js
@@ -0,0 +1,415 @@
+const { test } = require('brittle')
+const sinon = require('sinon')
+
+const { PaymentState, PAYMENT_STATE, PLUGIN_STATE, ERRORS } = require('../../src/payments/PaymentState')
+const update = sinon.stub()
+const uninitializedPayment = {
+  update,
+  db: { ready: true }
+}
+
+test('PaymentState.validate', t => {
+  t.exception(() => PaymentState.validate(), /Payment is required/)
+  t.exception(() => PaymentState.validate({}), /Payment db is required/)
+  t.exception(() => PaymentState.validate({ db: {} }), /Payment db is not ready/)
+
+  t.execution(() => PaymentState.validate(uninitializedPayment))
+})
+
+test('PaymentState.constructor', t => {
+  const pS = new PaymentState(uninitializedPayment)
+
+  t.is(pS.internalState, PAYMENT_STATE.INITIAL)
+  t.alike(pS.pendingPlugins, [])
+  t.alike(pS.triedPlugins, [])
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin, null)
+})
+
+test('PaymentState.assignPendingPlugins', t => {
+  const pS = new PaymentState(uninitializedPayment)
+  pS.assignPendingPlugins(['plugin0', 'plugin1', 'plugin2'])
+  t.alike(pS.pendingPlugins, ['plugin0', 'plugin1', 'plugin2'])
+})
+
+test('PaymentState.serialize', t => {
+  const pS = new PaymentState(uninitializedPayment)
+  t.alike(pS.serialize(), {
+    internalState: PAYMENT_STATE.INITIAL,
+    pendingPlugins: [],
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {}
+  })
+})
+
+test('PaymentState.currentState', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.currentState(), PAYMENT_STATE.INITIAL)
+
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  t.is(pS.currentState(), PAYMENT_STATE.IN_PROGRESS)
+})
+
+test('PaymentState.isInitial', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isInitial(), true)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  t.is(pS.isInitial(), false)
+})
+
+test('PaymentState.isInProgress', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isInProgress(), false)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  t.is(pS.isInProgress(), true)
+})
+
+test('PaymentState.isCompleted', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isCompleted(), false)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.COMPLETED
+  t.is(pS.isCompleted(), true)
+})
+
+test('PaymentState.isFailed', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isFailed(), false)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.FAILED
+  t.is(pS.isFailed(), true)
+})
+
+test('PaymentState.isCancelled', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isCancelled(), false)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.CANCELLED
+  t.is(pS.isCancelled(), true)
+})
+
+test('PaymentState.isFinal', t => {
+  let pS = new PaymentState(uninitializedPayment)
+  t.is(pS.isFinal(), false)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.COMPLETED
+  t.is(pS.isFinal(), true)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.FAILED
+  t.is(pS.isFinal(), true)
+  pS = new PaymentState(uninitializedPayment)
+  pS.internalState = PAYMENT_STATE.CANCELLED
+  t.is(pS.isFinal(), true)
+})
+
+test('PaymentState.cancel', async t => {
+  const pS = new PaymentState(uninitializedPayment)
+  await pS.cancel()
+
+  t.is(pS.internalState, PAYMENT_STATE.CANCELLED)
+  t.is(update.callCount, 1)
+
+  await t.exception(async () => await pS.cancel(), ERRORS.INVALID_STATE(PAYMENT_STATE.CANCELLED))
+
+  t.teardown(() => update.resetHistory())
+})
+
+test('PaymentState.failCurrentPlugin', async t => {
+  const pS = new PaymentState(uninitializedPayment)
+  await t.exception(async () => await pS.failCurrentPlugin(), ERRORS.INVALID_STATE(PAYMENT_STATE.INITIAL))
+
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  await t.exception(async () => await pS.failCurrentPlugin(), 'No current plugin')
+
+  pS.currentPlugin = { name: 'plugin0', startAt: Date.now() }
+  await pS.failCurrentPlugin()
+
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin, null)
+  t.is(pS.triedPlugins.length, 1)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt <= Date.now())
+  t.ok(pS.triedPlugins[0].endAt <= Date.now())
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(update.callCount, 1)
+
+  t.teardown(() => update.resetHistory())
+})
+
+test('PaymentState.fail', async t => {
+  let pS
+  const initializedPayment = {
+    update,
+    db: { ready: true },
+    pendingPlugins: ['plugin0', 'plugin1', 'plugin2', 'plugin3'],
+    triedPlugins: [],
+    currentPlugin: null,
+    sentByPlugin: null
+  }
+
+  pS = new PaymentState(initializedPayment)
+  await t.exception(async () => await pS.fail(), ERRORS.INVALID_STATE(PAYMENT_STATE.INITIAL))
+
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  pS.currentPlugin = { name: 'pluginX', startAt: Date.now() }
+  await pS.fail()
+
+  t.is(update.callCount, 2)
+  t.is(pS.internalState, PAYMENT_STATE.FAILED)
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin, null)
+  t.is(pS.triedPlugins.length, 1)
+  t.is(pS.triedPlugins[0].name, 'pluginX')
+  t.ok(pS.triedPlugins[0].startAt <= Date.now())
+  t.ok(pS.triedPlugins[0].endAt <= Date.now())
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+
+  pS = new PaymentState(initializedPayment)
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+  await pS.fail()
+
+  t.is(pS.internalState, PAYMENT_STATE.FAILED)
+  t.is(update.callCount, 3)
+
+  await t.exception(async () => await pS.fail(), ERRORS.INVALID_STATE(PAYMENT_STATE.FAILED))
+
+  pS = new PaymentState(uninitializedPayment)
+  await t.exception(async () => await pS.fail(), ERRORS.INVALID_STATE(PAYMENT_STATE.INITIAL))
+
+  t.teardown(() => update.resetHistory())
+})
+
+test('PaymentState.tryNext', async t => {
+  const initializedPayment = {
+    update,
+    db: { ready: true },
+    pendingPlugins: ['plugin0', 'plugin1', 'plugin2', 'plugin3'],
+    triedPlugins: [],
+    currentPlugin: null,
+    sentByPlugin: null
+  }
+
+  const pS = new PaymentState(initializedPayment)
+  t.alike(pS.pendingPlugins, ['plugin0', 'plugin1', 'plugin2', 'plugin3'])
+  t.alike(pS.triedPlugins, [])
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 0)
+  t.is(pS.internalState, PAYMENT_STATE.INITIAL)
+
+  await t.exception(async () => await pS.tryNext(), ERRORS.INVALID_STATE(PAYMENT_STATE.INITIAL))
+  pS.internalState = PAYMENT_STATE.IN_PROGRESS
+
+  const start0 = Date.now()
+  await pS.tryNext()
+
+  t.alike(pS.pendingPlugins, ['plugin1', 'plugin2', 'plugin3'])
+  t.is(pS.currentPlugin.name, 'plugin0')
+  t.ok(pS.currentPlugin.startAt >= start0)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.alike(pS.triedPlugins, [])
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 1)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  await t.exception(async () => await pS.tryNext(), ERRORS.PLUGIN_IN_PROGRESS('plugin0'))
+  await pS.failCurrentPlugin()
+  t.is(update.callCount, 2)
+
+  const start1 = Date.now()
+  await pS.tryNext()
+
+  t.alike(pS.pendingPlugins, ['plugin2', 'plugin3'])
+  t.is(pS.currentPlugin.name, 'plugin1')
+  t.ok(pS.currentPlugin.startAt >= start1)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.triedPlugins.length, 1)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt >= start0)
+  t.ok(pS.triedPlugins[0].endAt <= start1)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 3)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  t.teardown(() => update.resetHistory())
+})
+
+test('PaymentState.process', async t => {
+  const initializedPayment = {
+    update,
+    db: { ready: true },
+    pendingPlugins: ['plugin0', 'plugin1', 'plugin2', 'plugin3'],
+    triedPlugins: [],
+    currentPlugin: null,
+    sentByPlugin: null
+  }
+
+  const pS = new PaymentState(initializedPayment)
+  t.alike(pS.pendingPlugins, ['plugin0', 'plugin1', 'plugin2', 'plugin3'])
+  t.alike(pS.triedPlugins, [])
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 0)
+  t.is(pS.internalState, PAYMENT_STATE.INITIAL)
+
+  const start0 = Date.now()
+  await pS.process()
+
+  t.alike(pS.pendingPlugins, ['plugin1', 'plugin2', 'plugin3'])
+  t.is(pS.currentPlugin.name, 'plugin0')
+  t.ok(pS.currentPlugin.startAt >= start0)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.currentPlugin.state, PLUGIN_STATE.SUBMITTED)
+  t.alike(pS.triedPlugins, [])
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 2)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  await t.exception(async () => await pS.process(), ERRORS.PLUGIN_IN_PROGRESS('plugin0'))
+  await pS.failCurrentPlugin()
+  t.is(update.callCount, 3)
+
+  const start1 = Date.now()
+  await pS.process()
+
+  t.alike(pS.pendingPlugins, ['plugin2', 'plugin3'])
+  t.is(pS.currentPlugin.name, 'plugin1')
+  t.ok(pS.currentPlugin.startAt >= start1)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.currentPlugin.state, PLUGIN_STATE.SUBMITTED)
+  t.is(pS.triedPlugins.length, 1)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt >= start0)
+  t.ok(pS.triedPlugins[0].endAt <= start1)
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 4)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  await t.exception(async () => await pS.process(), ERRORS.PLUGIN_IN_PROGRESS('plugin1'))
+  await pS.failCurrentPlugin()
+  t.is(update.callCount, 5)
+
+  const start2 = Date.now()
+  await pS.process()
+
+  t.alike(pS.pendingPlugins, ['plugin3'])
+  t.is(pS.currentPlugin.name, 'plugin2')
+  t.ok(pS.currentPlugin.startAt >= start2)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.currentPlugin.state, PLUGIN_STATE.SUBMITTED)
+  t.is(pS.triedPlugins.length, 2)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt >= start0)
+  t.ok(pS.triedPlugins[0].endAt <= start1)
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[1].name, 'plugin1')
+  t.ok(pS.triedPlugins[1].startAt >= start1)
+  t.ok(pS.triedPlugins[1].endAt <= start2)
+  t.is(pS.triedPlugins[1].state, PLUGIN_STATE.FAILED)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 6)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  await t.exception(async () => await pS.process(), ERRORS.PLUGIN_IN_PROGRESS('plugin2'))
+  await pS.failCurrentPlugin()
+  t.is(update.callCount, 7)
+
+  const start3 = Date.now()
+  await pS.process()
+
+  t.alike(pS.pendingPlugins, [])
+  t.is(pS.currentPlugin.name, 'plugin3')
+  t.ok(pS.currentPlugin.startAt >= start3)
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.currentPlugin.state, PLUGIN_STATE.SUBMITTED)
+  t.is(pS.triedPlugins.length, 3)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt >= start0)
+  t.ok(pS.triedPlugins[0].endAt <= start1)
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[1].name, 'plugin1')
+  t.ok(pS.triedPlugins[1].startAt >= start1)
+  t.ok(pS.triedPlugins[1].endAt <= start2)
+  t.is(pS.triedPlugins[1].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[2].name, 'plugin2')
+  t.ok(pS.triedPlugins[2].startAt >= start2)
+  t.ok(pS.triedPlugins[2].endAt <= start3)
+  t.is(pS.triedPlugins[2].state, PLUGIN_STATE.FAILED)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 8)
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  await t.exception(async () => await pS.process(), ERRORS.PLUGIN_IN_PROGRESS('plugin3'))
+  await pS.failCurrentPlugin()
+  t.is(update.callCount, 9)
+
+  const start4 = Date.now()
+  await pS.process()
+
+  t.alike(pS.pendingPlugins, [])
+  t.is(pS.currentPlugin, null)
+  t.is(pS.triedPlugins.length, 4)
+  t.is(pS.triedPlugins[0].name, 'plugin0')
+  t.ok(pS.triedPlugins[0].startAt >= start0)
+  t.ok(pS.triedPlugins[0].endAt <= start1)
+  t.is(pS.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[1].name, 'plugin1')
+  t.ok(pS.triedPlugins[1].startAt >= start1)
+  t.ok(pS.triedPlugins[1].endAt <= start2)
+  t.is(pS.triedPlugins[1].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[2].name, 'plugin2')
+  t.ok(pS.triedPlugins[2].startAt >= start2)
+  t.ok(pS.triedPlugins[2].endAt <= start3)
+  t.is(pS.triedPlugins[2].state, PLUGIN_STATE.FAILED)
+  t.is(pS.triedPlugins[3].name, 'plugin3')
+  t.ok(pS.triedPlugins[3].startAt >= start3)
+  t.ok(pS.triedPlugins[3].endAt <= start4)
+  t.is(pS.triedPlugins[3].state, PLUGIN_STATE.FAILED)
+  t.is(pS.sentByPlugin, null)
+  t.is(update.callCount, 10)
+  t.is(pS.internalState, PAYMENT_STATE.FAILED)
+
+  t.teardown(() => update.resetHistory())
+})
+
+test('PaymentState.complete', async t => {
+  const initializedPayment = {
+    update,
+    db: { ready: true },
+    pendingPlugins: ['plugin0', 'plugin1', 'plugin2', 'plugin3'],
+    triedPlugins: [],
+    currentPlugin: null,
+    sentByPlugin: null
+  }
+
+  const pS = new PaymentState(initializedPayment)
+
+  await t.exception(async () => await pS.complete(), ERRORS.INVALID_PAYMENT_STATE)
+
+  await pS.process()
+
+  t.is(pS.internalState, PAYMENT_STATE.IN_PROGRESS)
+  t.is(update.callCount, 2)
+  t.is(pS.currentPlugin.name, 'plugin0')
+  t.ok(pS.currentPlugin.startAt <= Date.now())
+  t.is(pS.currentPlugin.state, PLUGIN_STATE.SUBMITTED)
+  t.is(pS.sentByPlugin, null)
+
+  await pS.complete()
+
+  t.is(pS.internalState, PAYMENT_STATE.COMPLETED)
+  t.is(update.callCount, 3)
+  t.is(pS.currentPlugin, null)
+  t.is(pS.sentByPlugin.name, 'plugin0')
+  t.ok(pS.sentByPlugin.startAt <= Date.now())
+  t.ok(pS.sentByPlugin.endAt <= Date.now())
+  t.is(pS.sentByPlugin.state, PLUGIN_STATE.SUCCESS)
+
+  t.teardown(() => update.resetHistory())
+})
