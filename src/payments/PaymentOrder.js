f989efa17fab7909645248b0977c32e647604ec2
dzdidi <deniszalessky@gmail.com>
2023-05-24 15:52:38 +0200

    PaymentOrder: initial
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ src/payments/PaymentOrder.js
@@ -0,0 +1,332 @@
+const { Payment } = require('./Payment')
+const { PaymentAmount } = require('./PaymentAmount')
+/**
+ * Payment Order class
+ * @class PaymentOrder - This class is used to create a payments
+ * @property {string} id - Order id
+ * @property {string} clientOrderId - Client order id
+ * @property {string} type - Order type
+ * @property {string} state - Order state
+ * @property {string} frequency - Order frequency
+ * @property {Payment[]} payments - Payments associated with this order
+ * @property {PaymentAmount} amount - Payment amount
+ * @property {string} targetURL - Target URL
+ * @property {string} memo - Memo
+ * @property {string} sendingPriority - Sending priority
+ * @property {object} orderParams - Order params
+ * @property {object} db - Database
+ */
+
+class PaymentOrder {
+  static generateId () {
+    return 'totally-random-order-id'
+  }
+
+  /**
+   * @constructor - PaymentOrder constructor
+   * @param {object} orderParams - Order params
+   * @param {object} db - Database
+   * @returns {PaymentOrder}
+   */
+  constructor (orderParams, db) {
+    this.orderParams = orderParams
+    this.db = db
+
+    this.id = orderParams.id || null
+    this.clientOrderId = orderParams.clientOrderId
+
+    this.state = orderParams.state || ORDER_STATE.CREATED
+
+    this.clientOrderId = orderParams.clientOrderId
+    this.type = orderParams.type || ORDER_TYPE.ONE_TIME
+    if (this.type === ORDER_TYPE.ONE_TIME) {
+      this.frequency = null
+    } else {
+      this.frequency = orderParams.frequency
+    }
+
+    this.payments = []
+
+    this.amount = new PaymentAmount(orderParams)
+    this.targetURL = orderParams.targetURL
+    this.memo = orderParams.memo || ''
+    this.sendingPriority = orderParams.sendingPriority
+
+    if (this.type === ORDER_TYPE.RECURRING) {
+      throw new Error(ERRORS.NOT_IMPLEMENTED)
+    }
+  }
+
+  /**
+   * @method init - Initialize order and create payments
+   * @returns {Promise<void>}
+   */
+  async init () {
+    this.id = PaymentOrder.generateId()
+    // TODO: check if order with this.clientOrderId already exists
+    this.state = ORDER_STATE.INITIALIZED
+    if (this.orderParams.type === ORDER_TYPE.RECURRING) {
+      await this.createRecurringOrder()
+    } else {
+      await this.createOneTimeOrder()
+    }
+
+    this.save()
+  }
+
+  /**
+   * Crate one time order
+   * @returns {Promise<void>}
+   */
+  async createOneTimeOrder () {
+    const payment = new Payment(
+      { ...this.orderParams, orderId: this.id },
+      this.db
+    )
+    await payment.init()
+
+    this.payments.push(payment)
+  }
+
+  async createRecurringOrder () {
+    // TODO: save order and payments to db in a single transaction
+    throw new Error(ERRORS.NOT_IMPLEMENTED)
+  }
+
+  /**
+   * @method process - Process order
+   * @returns {Promise<Payment>}
+   */
+  async process () {
+    if (!this.canProcess()) throw new Error(ERRORS.CAN_NOT_PROCESS_ORDER)
+
+    const paymentInProgress = this.getPaymentInProgress()
+    if (paymentInProgress) return await paymentInProgress.process()
+
+    // TODO: refactor this if completion is moved out of this class
+    // by moving getFirstOutstandingPayment() to processPayment
+    const payment = this.getFirstOutstandingPayment()
+    if (payment) {
+      return await this.processPayment(payment)
+    } else {
+      // XXX: consider moving out of this class?
+      return await this.complete()
+    }
+  }
+
+  /**
+   * Checks if order is ready to be processed
+   * @method canProcess
+   * @returns {boolean}
+   */
+  canProcess () {
+    return this.state === ORDER_STATE.INITIALIZED || this.state === ORDER_STATE.PROCESSING
+  }
+
+  /**
+   * @method processPayment - Process payment
+   * @param {Payment} payment - Payment to process
+   * @returns {Promise<Payment>}
+   */
+  async processPayment (payment) {
+    if (payment.executeAt > Date.now()) return payment
+
+    if (this.state !== ORDER_STATE.PROCESSING) {
+      this.state = ORDER_STATE.PROCESSING
+      await this.update()
+    }
+
+    return await payment.process()
+  }
+
+  /**
+   * @method getFirstOutstandingPayment - Get first outstanding payment
+   * @returns {Payment}
+   */
+  getFirstOutstandingPayment () {
+    return this.payments.find((payment) => !payment.isFinal())
+  }
+
+  /**
+   * @method getPaymentInProgress - Get payment in progress
+   * @returns {Payment}
+   */
+  getPaymentInProgress () {
+    return this.payments.find((payment) => payment.isInProgress())
+  }
+
+  /**
+   * @method complete - Complete order
+   * @throws {Error} - If order is already completed
+   * @throws {Error} - If order is cancelled
+   * @returns {Promise<Payment>} - Last payment
+   */
+  async complete () {
+    if (this.state === ORDER_STATE.CANCELLED) throw new Error(ERRORS.ORDER_CANCELLED)
+    if (this.state === ORDER_STATE.COMPLETED) throw new Error(ERRORS.ORDER_COMPLETED)
+
+    if (this.payments.every((payment) => payment.internalState.isFinal())) {
+      this.state = ORDER_STATE.COMPLETED
+      await this.update()
+    } else {
+      throw new Error(ERRORS.OUTSTANDING_PAYMENTS)
+    }
+
+    return this.payments[this.payments.length - 1]
+  }
+
+  /**
+   * @method cancel - Cancel order and all outstanding payments
+   * @throws {Error} - If order is already completed
+   * @returns {Promise<void>}
+   */
+  async cancel () {
+    if (this.state === ORDER_STATE.COMPLETED) {
+      throw new Error(ERRORS.ORDER_COMPLETED)
+    }
+
+    // TODO: db transaction
+    await this.payments
+      .filter((payment) => !payment.isFinal())
+      .forEach(async (payment) => {
+        await payment.cancel()
+      })
+
+    this.state = ORDER_STATE.CANCELLED
+    await this.update()
+  }
+
+  /**
+   * @method serialize - serialize order
+   * @returns {Object}
+   */
+  serialize () {
+    return {
+      id: this.id,
+      clientOrderId: this.clientOrderId,
+      type: this.type,
+      state: this.state,
+      frequency: this.frequency,
+
+      targetURL: this.targetURL,
+      memo: this.memo,
+      sendingPriority: this.sendingPriority,
+      ...this.amount.serialize()
+    }
+  }
+
+  /**
+   * @method save - Save order with all corresponding payments to db
+   * @returns {Promise<void>}
+   */
+  async save () {
+    // TODO: needs to be more sophisticated than this
+    // check if payment already exists as well
+    // save corresponding payments
+    //    if (this.id) {
+    //      const order = await this.db.get(this.id, { removed: '*' })
+    //      if (order) throw new Error(ERRORS.ALREADY_EXISTS(this.id))
+    //      // something very fishy is going on
+    //    }
+    //
+    //    this.id = PaymentOrder.generateId()
+    const orderObject = this.serialize()
+    // Order.validateOrderObject(orderObject)
+
+    // TODO: db transaction
+    await this.db.save(orderObject)
+    await Promise.all(this.payments.map(async (payment) => {
+      await payment.save()
+    }))
+  }
+
+  /**
+   * @method update - Update order in db
+   * @returns {Promise<void>}
+   */
+  async update () {
+    // TODO: add some validation
+    // also check for state and existing payments associated with this order
+    const serialized = this.serialize()
+    // Payment.validatePaymentObject(serialized)
+    await this.db.update(this.id, serialized)
+  }
+
+  /**
+   * @static find - Find order by id in db
+   * @param {string} id - Order id
+   * @param {DB} db - DB instance
+   * @returns {Promise<PaymentOrder>}
+   */
+  static async find (id, db) {
+    const orderParams = await db.get(id)
+    if (!orderParams) throw new Error(ERRORS.ORDER_NOT_FOUND(id))
+
+    const paymentOrder = new PaymentOrder(orderParams, db)
+    paymentOrder.payments = (await db.getPayments(id)).map(p => new Payment(p, db))
+
+    return paymentOrder
+  }
+}
+
+/**
+ * @typedef {Obejct} ERRROS
+ * @property {string} NOT_IMPLEMENTED
+ * @property {string} ORDER_PARAMS_REQUIRED
+ * @property {string} ORDER_AMOUNT_REQUIRED
+ * @property {string} ORDER_TARGET_URL_REQUIRED
+ * @property {string} ORDER_CLIENT_ORDER_ID_REQUIRED
+ * @property {string} ORDER_CONFIG_REQUIRED
+ * @property {string} ORDER_CONFIG_SENDING_PARTY_REQUIRED
+ * @property {string} DB_REQUIRED
+ * @property {string} DB_NOT_READY
+ * @property {string} OUTSTANDING_PAYMENTS
+ * @property {string} ORDER_CANCELLED
+ * @property {string} ORDER_COMPLETED
+ * @property {string} CAN_NOT_PROCESS_ORDER
+ * @property {function} ORDER_NOT_FOUND
+ */
+const ERRORS = {
+  NOT_IMPLEMENTED: 'Not implemented',
+  ORDER_PARAMS_REQUIRED: 'Order params are required',
+  ORDER_AMOUNT_REQUIRED: 'Order amount is required',
+  ORDER_TARGET_URL_REQUIRED: 'Order target url is required',
+  ORDER_CLIENT_ORDER_ID_REQUIRED: 'Order client order id is required',
+  ORDER_CONFIG_REQUIRED: 'Order config is required',
+  ORDER_CONFIG_SENDING_PARTY_REQUIRED: 'Order config sending party is required',
+  DB_REQUIRED: 'DB is required',
+  DB_NOT_READY: 'DB is not ready',
+  OUTSTANDING_PAYMENTS: 'There are outstanding payments',
+  ORDER_CANCELLED: 'Order is cancelled',
+  ORDER_COMPLETED: 'Order is completed',
+  CAN_NOT_PROCESS_ORDER: 'Can not process order',
+  ORDER_NOT_FOUND: (id) => `Order with id ${id} not found`
+}
+
+/**
+ * @typedef {Object} ORDER_TYPE
+ * @property {string} ONE_TIME
+ * @property {string} RECURRING
+ */
+const ORDER_TYPE = {
+  ONE_TIME: 'one-time',
+  RECURRING: 'recurring'
+}
+
+/**
+ * @typedef {Object} ORDER_STATE
+ * @property {string} CREATED
+ * @property {string} INITIALIZED
+ * @property {string} PROCESSING
+ * @property {string} COMPLETED
+ * @property {string} CANCELLED
+ */
+const ORDER_STATE = {
+  CREATED: 'created',
+  INITIALIZED: 'initialized',
+  PROCESSING: 'processing',
+  COMPLETED: 'completed',
+  CANCELLED: 'cancelled'
+}
+
+module.exports = { PaymentOrder, ORDER_TYPE, ORDER_STATE, ERRORS }
---
+++ test/payments/PaymentOrder.test.js
@@ -0,0 +1,418 @@
+const { test } = require('brittle')
+const { DB } = require('../../src/DB')
+
+const { PLUGIN_STATE, PAYMENT_STATE } = require('../../src/payments/PaymentState')
+const { PaymentAmount } = require('../../src/payments/PaymentAmount')
+
+const { orderParams } = require('../fixtures/paymentParams')
+
+const { PaymentOrder, ORDER_TYPE, ORDER_STATE, ERRORS } = require('../../src/payments/PaymentOrder')
+
+async function getPaymentOrderInstance () {
+  const db = new DB()
+  await db.init()
+
+  const params = { ...orderParams }
+
+  return new PaymentOrder(params, db)
+}
+
+test('PaymentOrder - new (default type)', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  t.alike(paymentOrder.orderParams, orderParams)
+  t.is(paymentOrder.clientOrderId, orderParams.clientOrderId)
+  t.is(paymentOrder.type, orderParams.type || ORDER_TYPE.ONE_TIME)
+  t.alike(paymentOrder.payments, [])
+  t.is(paymentOrder.frequency, null)
+})
+
+test('PaymentOrder - new (one time)', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  t.alike(paymentOrder.orderParams, orderParams)
+  t.is(paymentOrder.clientOrderId, orderParams.clientOrderId)
+  t.is(paymentOrder.type, orderParams.type || ORDER_TYPE.ONE_TIME)
+  t.alike(paymentOrder.payments, [])
+
+  t.is(paymentOrder.frequency, null)
+
+  t.is(paymentOrder.state, ORDER_STATE.CREATED)
+
+  t.alike(paymentOrder.amount, new PaymentAmount(orderParams))
+  t.is(paymentOrder.targetURL, orderParams.targetURL)
+  t.is(paymentOrder.memo, orderParams.memo || '')
+})
+
+test('PaymentOrder - new (recurring)', async t => {
+  const db = new DB()
+  await db.init()
+
+  const params = { ...orderParams, type: ORDER_TYPE.RECURRING }
+
+  t.exception(() => { new PaymentOrder(params, db) }, ERRORS.NOT_IMPLEMENTED) // eslint-disable-line 
+})
+
+test('PaymentOrder.init', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  t.absent(paymentOrder.id)
+  await paymentOrder.init()
+
+  t.ok(paymentOrder.id)
+  t.is(paymentOrder.state, ORDER_STATE.INITIALIZED)
+
+  const got = await paymentOrder.db.get(paymentOrder.id)
+  t.alike(got, {
+    id: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    type: 'one-time',
+    state: ORDER_STATE.INITIALIZED,
+    frequency: null,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE'
+  })
+
+  t.is(paymentOrder.payments.length, 1)
+  t.alike(paymentOrder.payments[0].serialize(), {
+    id: 'totally-random-id',
+    orderId: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE',
+    internalState: PAYMENT_STATE.INITIAL,
+    pendingPlugins: orderParams.sendingPriority,
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {},
+    createdAt: paymentOrder.payments[0].createdAt,
+    executeAt: paymentOrder.payments[0].executeAt
+  })
+})
+
+test('PaymentOrder.serialize', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  const serialized = paymentOrder.serialize()
+  t.alike(serialized, {
+    id: null,
+    clientOrderId: orderParams.clientOrderId,
+    type: ORDER_TYPE.ONE_TIME,
+    frequency: null,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE',
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    state: ORDER_STATE.CREATED
+  })
+})
+
+test('PaymentOrder.save', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+
+  t.ok(paymentOrder.id)
+  const gotOrder = await paymentOrder.db.get(paymentOrder.id)
+  t.alike(gotOrder, {
+    id: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    type: 'one-time',
+    state: ORDER_STATE.INITIALIZED,
+    frequency: null,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE'
+  })
+
+  const gotPayment = await paymentOrder.db.get(paymentOrder.payments[0].id)
+  t.alike(gotPayment, {
+    id: 'totally-random-id',
+    orderId: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE',
+    internalState: PAYMENT_STATE.INITIAL,
+    pendingPlugins: orderParams.sendingPriority,
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {},
+    createdAt: paymentOrder.payments[0].createdAt,
+    executeAt: paymentOrder.payments[0].executeAt
+  })
+})
+
+test('PaymentOrder.update', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+  let got
+
+  t.ok(paymentOrder.id)
+  got = await paymentOrder.db.get(paymentOrder.id)
+  t.is(got.amount, '100')
+
+  paymentOrder.amount = new PaymentAmount({ amount: '101' })
+  await paymentOrder.update()
+
+  got = await paymentOrder.db.get(paymentOrder.id)
+
+  t.alike(got, paymentOrder.serialize())
+  t.is(got.amount, '101')
+})
+
+test('PaymentOrder.getFirstOutstandingPayment', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  t.absent(paymentOrder.getFirstOutstandingPayment())
+
+  await paymentOrder.init()
+  t.alike(paymentOrder.getFirstOutstandingPayment(), paymentOrder.payments[0])
+})
+
+test('PaymentOrder.getPaymentInProgress', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  t.absent(paymentOrder.getPaymentInProgress())
+
+  await paymentOrder.init()
+  t.absent(paymentOrder.getPaymentInProgress())
+
+  const payment = await paymentOrder.process()
+  t.alike(paymentOrder.getPaymentInProgress(), payment)
+})
+
+test('PaymentOrder.canProcess', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  t.not(paymentOrder.canProcess())
+
+  await paymentOrder.init()
+  t.ok(paymentOrder.canProcess())
+})
+
+test('PaymentOrder.createRecurringOrder', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  await t.exception(async () => await paymentOrder.createRecurringOrder(), ERRORS.NOT_IMPLEMENTED)
+})
+
+test('PaymentOrder.createOneTimeOrder', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  await t.exception(async () => await paymentOrder.createOneTimeOrder(), ERRORS.ORDER_ID_REQUIRED)
+
+  paymentOrder.id = '1234'
+  await paymentOrder.createOneTimeOrder()
+
+  t.is(paymentOrder.payments.length, 1)
+
+  const payment = paymentOrder.payments[0].serialize()
+  t.alike(payment, {
+    id: null,
+    orderId: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE',
+    internalState: PAYMENT_STATE.INITIAL,
+    pendingPlugins: ['p2sh', 'p2tr'],
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {},
+    createdAt: paymentOrder.payments[0].createdAt,
+    executeAt: paymentOrder.payments[0].executeAt
+  })
+})
+
+test('PaymentOrder.processPayment', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+
+  await t.exception(async () => await paymentOrder.createOneTimeOrder(), ERRORS.ORDER_ID_REQUIRED)
+
+  paymentOrder.id = '1234'
+  await paymentOrder.createOneTimeOrder()
+
+  t.is(paymentOrder.payments.length, 1)
+
+  const payment = paymentOrder.payments[0].serialize()
+  t.alike(payment, {
+    id: null,
+    orderId: paymentOrder.id,
+    clientOrderId: orderParams.clientOrderId,
+    targetURL: orderParams.targetURL,
+    memo: '',
+    sendingPriority: orderParams.sendingPriority,
+    amount: orderParams.amount,
+    currency: 'BTC',
+    denomination: 'BASE',
+    internalState: PAYMENT_STATE.INITIAL,
+    pendingPlugins: ['p2sh', 'p2tr'],
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {},
+    createdAt: paymentOrder.payments[0].createdAt,
+    executeAt: paymentOrder.payments[0].executeAt
+  })
+
+  payment.executeAt = new Date() + 10000
+  let res
+
+  res = await paymentOrder.processPayment(paymentOrder.payments[0])
+  t.alike(
+    { ...res.serialize(), executeAt: payment.executeAt },
+    { ...payment, internalState: PAYMENT_STATE.IN_PROGRESS }
+  )
+
+  payment.executeAt = new Date() - 10000
+  res = await paymentOrder.processPayment(paymentOrder.payments[0])
+  const serialized = res.serialize()
+
+  t.is(serialized.internalState, PAYMENT_STATE.IN_PROGRESS)
+  t.is(serialized.currentPlugin.name, 'p2sh')
+})
+
+test('PaymentOrder.complete', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+
+  t.is(paymentOrder.state, ORDER_STATE.INITIALIZED)
+
+  t.ok(paymentOrder.id)
+
+  paymentOrder.state = ORDER_STATE.CANCELLED
+  await paymentOrder.update()
+  await t.exception(async () => { await paymentOrder.complete() }, ERRORS.ORDER_CANCELLED)
+  t.is(paymentOrder.state, ORDER_STATE.CANCELLED)
+
+  paymentOrder.state = ORDER_STATE.INITIALIZED
+  await paymentOrder.update()
+
+  await t.exception(async () => { await paymentOrder.complete() }, ERRORS.OUTSTANDING_PAYMENTS)
+  t.is(paymentOrder.state, ORDER_STATE.INITIALIZED)
+
+  await paymentOrder.process()
+  await t.exception(async () => { await paymentOrder.complete() }, ERRORS.OUTSTANDING_PAYMENTS)
+  t.is(paymentOrder.state, ORDER_STATE.PROCESSING)
+
+  await paymentOrder.payments[0].internalState.complete()
+  await paymentOrder.complete()
+
+  t.is(paymentOrder.state, ORDER_STATE.COMPLETED)
+
+  await t.exception(async () => { await paymentOrder.complete() }, ERRORS.ORDER_COMPLETED)
+  t.is(paymentOrder.state, ORDER_STATE.COMPLETED)
+})
+
+test('PaymentOrder.process', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+  let payment
+  let serialized
+
+  t.ok(paymentOrder.id)
+  payment = await paymentOrder.process()
+  serialized = payment.serialize()
+  t.alike(serialized, paymentOrder.payments[0].serialize())
+
+  t.is(serialized.id, payment.id)
+  t.is(serialized.orderId, paymentOrder.id)
+  t.is(serialized.clientOrderId, orderParams.clientOrderId)
+  t.is(serialized.targetURL, orderParams.targetURL)
+  t.is(serialized.memo, '')
+  t.alike(serialized.sendingPriority, orderParams.sendingPriority)
+  t.is(serialized.amount, orderParams.amount)
+  t.is(serialized.currency, 'BTC')
+  t.is(serialized.denomination, 'BASE')
+  t.is(serialized.internalState, PAYMENT_STATE.IN_PROGRESS)
+  t.alike(serialized.pendingPlugins, ['p2tr'])
+  t.alike(serialized.triedPlugins, [])
+  t.is(serialized.currentPlugin.name, 'p2sh')
+  t.ok(serialized.currentPlugin.startAt <= Date.now())
+  t.absent(serialized.currentPlugin.endAt)
+
+  // same order
+  const paymentInProgress = await paymentOrder.process()
+  t.alike(paymentInProgress.serialize(), serialized)
+
+  await paymentOrder.payments[0].internalState.failCurrentPlugin()
+
+  payment = await paymentOrder.process()
+  serialized = payment.serialize()
+  t.alike(serialized, paymentOrder.payments[0].serialize())
+
+  t.is(serialized.id, payment.id)
+  t.is(serialized.orderId, paymentOrder.id)
+  t.is(serialized.clientOrderId, orderParams.clientOrderId)
+  t.is(serialized.targetURL, orderParams.targetURL)
+  t.is(serialized.memo, '')
+  t.alike(serialized.sendingPriority, orderParams.sendingPriority)
+  t.is(serialized.amount, orderParams.amount)
+  t.is(serialized.currency, 'BTC')
+  t.is(serialized.denomination, 'BASE')
+  t.is(serialized.internalState, PAYMENT_STATE.IN_PROGRESS)
+
+  t.alike(serialized.pendingPlugins, [])
+  t.is(serialized.triedPlugins.length, 1)
+  t.is(serialized.triedPlugins[0].name, 'p2sh')
+  t.ok(serialized.triedPlugins[0].startAt <= Date.now())
+  t.ok(serialized.triedPlugins[0].endAt <= Date.now())
+
+  t.is(serialized.currentPlugin.name, 'p2tr')
+  t.ok(serialized.currentPlugin.startAt <= Date.now())
+
+  await payment.complete()
+  payment = await paymentOrder.process()
+
+  t.is(paymentOrder.state, ORDER_STATE.COMPLETED)
+
+  serialized = payment.serialize()
+  t.is(serialized.internalState, PAYMENT_STATE.COMPLETED)
+  t.alike(serialized.currentPlugin, {})
+  t.alike(serialized.pendingPlugins, [])
+  t.is(serialized.triedPlugins.length, 2)
+  t.is(serialized.triedPlugins[0].name, 'p2sh')
+  t.is(serialized.triedPlugins[0].state, PLUGIN_STATE.FAILED)
+  t.is(serialized.triedPlugins[1].name, 'p2tr')
+  t.is(serialized.triedPlugins[1].state, PLUGIN_STATE.SUCCESS)
+  t.is(serialized.sentByPlugin.name, 'p2tr')
+  t.is(serialized.sentByPlugin.state, PLUGIN_STATE.SUCCESS)
+  t.ok(serialized.sentByPlugin.startAt <= Date.now())
+  t.ok(serialized.sentByPlugin.endAt <= Date.now())
+})
+
+test('PaymentOrder.cancel', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+
+  t.ok(paymentOrder.id)
+
+  await paymentOrder.cancel()
+
+  t.is(paymentOrder.state, ORDER_STATE.CANCELLED)
+  t.is(paymentOrder.payments[0].serialize().internalState, PAYMENT_STATE.CANCELLED)
+})
+
+test('PaymentOrder.find', async t => {
+  const paymentOrder = await getPaymentOrderInstance()
+  await paymentOrder.init()
+  const id = paymentOrder.id
+
+  const got = await PaymentOrder.find(id, paymentOrder.db)
+  t.alike(got.serialize(), paymentOrder.serialize())
+})
