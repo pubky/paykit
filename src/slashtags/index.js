8cac0505fcc4ca19d774202c219341a7bf5780d6
dzdidi <deniszalessky@gmail.com>
2023-06-01 09:20:00 +0200

    SlashtagsConnector: types annotation
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/slashtags/index.js
+++ src/slashtags/index.js
@@ -3,28 +3,28 @@ const b4a = require('b4a')
 const CoreData = require('@synonymdev/slashtags-core-data')
 const SlashtagsURL = require('@synonymdev/slashtags-url')
 
-const ERRORS = {
-  NOT_READY: 'SlashtagsConnector is not ready',
-  INVALID_JSON: 'Invalid JSON',
-  INVALID_URL: 'Invalid URL',
-  INDEX_NOT_FOUND: 'Index not found',
-  FILE_NOT_REFERENCED: 'File not referenced'
-}
-
-// XXX I do not like idea of signaling encrypted vs unencrypted data via path
+// NOTE: do not like idea of signaling encrypted vs unencrypted data via path
 const SLASHPAY_PATH = '/public/slashpay.json'
 
+/**
+ * SlashtagsConnector class
+ * @class SlashtagsConnector
+ * @param {Object} params - parameters for core data
+ * @property {CoreData} coreData - core data instance
+ * @property {boolean} ready - is SlashtagsConnector ready
+ */
 class SlashtagsConnector {
-  constructor (coreData) {
-    // Do we need it?
-    if (coreData instanceof CoreData) {
-      this.coreData = coreData
-    } else {
-      this.coreData = new CoreData(coreData)
-    }
+  constructor (params) {
+    this.coreData = new CoreData(params)
     this.ready = false
   }
 
+  /**
+   * Validate data
+   * @param {Object|string} data - data to validate
+   * @throws {Error} - if data is not valid JSON
+   * @throws {Error} - if data is empty object
+   */
   static validate (data) {
     if (!data) throw new Error(ERRORS.INVALID_JSON)
 
@@ -42,11 +42,24 @@ class SlashtagsConnector {
     }
   }
 
+  /**
+   * Initialize SlashtagsConnector
+   * @returns {Promise<void>}
+   */
   async init () {
+    if (this.ready) return
+
     await this.coreData.ready()
     this.ready = true
   }
 
+  /**
+   * Read a file from local drive
+   * @param {string} path - path to the file
+   * @returns {Promise<Object|null>} - content of the file or null
+   * @throws {Error} - if SlashtagsConnector is not ready
+   * @throws {Error} - if path is not valid
+   */
   async readLocal (path = SLASHPAY_PATH) {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
 
@@ -54,6 +67,14 @@ class SlashtagsConnector {
     return buf && decode(buf)
   }
 
+  /**
+   * Read a file from remote drive
+   * @param {string} url - url to the file
+   * @param {Object} opts
+   * @returns {Promise<Object|null>} - content of the file or null
+   * @throws {Error} - if SlashtagsConnector is not ready
+   * @throws {Error} - if url is not valid
+   */
   async readRemote (url, opts = {}) {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
 
@@ -70,23 +91,46 @@ class SlashtagsConnector {
     return buf && decode(buf)
   }
 
+  /**
+   * Read a file
+   * @param {string} key - path to file
+   * @param {Object} value - object to be stored
+   * @param {Object} opts
+   * @returns {Promise<string>} - url to the file
+   * @throws {Error} - if SlashtagsConnector is not ready
+   * @throws {Error} - if value is not valid JSON
+   */
   async create (key, value, opts = {}) {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
     SlashtagsConnector.validate(value)
 
     await this.coreData.create(key, encode(value), opts)
 
-    // XXX url should be an object to support `join` and `toString`
+    // NOTE: url should be an object to support `join` and `toString`
     // if path is not public, key should be included
     return this.coreData.url + key
   }
 
+  /**
+   * Get url to a drive
+   * @returns {string}
+   * @throws {Error} - if SlashtagsConnector is not ready
+   */
   getUrl () {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
 
     return this.coreData.url
   }
 
+  /**
+   * Update a file
+   * @param {string} key - path to file
+   * @param {Object} value - new value
+   * @param {Object} opts
+   * @returns {Promise<void>}
+   * @throws {Error} - if SlashtagsConnector is not ready
+   * @throws {Error} - if value is not valid JSON
+   */
   async update (key, value, opts = {}) {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
     SlashtagsConnector.validate(value)
@@ -94,6 +138,15 @@ class SlashtagsConnector {
     await this.coreData.update(key, encode(value), opts)
   }
 
+  /**
+   * Delete a file or all files
+   * @param {string} key - path to file
+   * @param {Object} opts
+   * @returns {Promise<void>}
+   * @throws {Error} - if SlashtagsConnector is not ready
+   * @throws {Error} - if index is not found
+   * @throws {Error} - if file is not referenced in index
+   */
   async delete (key = SLASHPAY_PATH, opts = {}) {
     if (!this.ready) throw new Error(ERRORS.NOT_READY)
 
@@ -117,9 +170,28 @@ class SlashtagsConnector {
     await this.update(SLASHPAY_PATH, index, opts)
   }
 
+  /**
+   * Close the connection to the underlying storage.
+   * @returns {Promise<void>}
+   */
   async close () {
     await this.coreData.close()
   }
+}
+/**
+ * @typedef {Object} Error
+ * @property {string} NOT_READY - SlashtagsConnector is not ready
+ * @property {string} INVALID_JSON - Invalid JSON
+ * @property {string} INVALID_URL - Invalid URL
+ * @property {string} INDEX_NOT_FOUND - Index not found
+ * @property {string} FILE_NOT_REFERENCED - File not referenced
+ */
+const ERRORS = {
+  NOT_READY: 'SlashtagsConnector is not ready',
+  INVALID_JSON: 'Invalid JSON',
+  INVALID_URL: 'Invalid URL',
+  INDEX_NOT_FOUND: 'Index not found',
+  FILE_NOT_REFERENCED: 'File not referenced'
 }
 
 /**
