e21f6dd509990d0cedf63cd4e7504599496cf98e
dzdidi <deniszalessky@gmail.com>
2023-06-26 10:09:18 +0200

    PaymentReceiver: initial
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ src/payments/PaymentReceiver.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const { Payment } = require('./Payment')
+/**
+ * PaymentReceiver is a class which is responsible for making plugins to receive payments
+ * @class PaymentReceiver
+ */
+class PaymentReceiver {
+  /**
+   * @constructor PaymentReceiver
+   * @param {DB} db - instance of a database
+   * @param {PluginManager} pluginManager - instance of a plugin manager
+   * @param {RemoteStorage} storage - instance of a local storage (e.g. HyperDrive)
+   * @param {Function} notificationCallback - callback which is called when payment is received
+   */
+  constructor (db, pluginManager, storage, notificationCallback) {
+    this.db = db // internal state storage
+    this.storage = storage // internal public interface
+    this.notificationCallback = notificationCallback
+    this.pluginManager = pluginManager
+  }
+
+
+
+  // TODO: allow for initialization of receiving payment with specified amount. Logically this can be introduced as
+  // as one-time-incoming payment with amount validation happening here, as some plugins may not support such.
+  // Slashpay.json for such payments should be stored in a separate core in order to no
+  //
+  //
+  //
+  // TODO: make sure slashtags-data works with full spectrum of  private/public vs persisted/ephemeral
+
+  /**
+   * Initialize, get ready to receive payments at returned URL
+   * @returns {Promise<String>} - url to local drive where slashpay.json file is located
+   */
+  async init () {
+    const paymentPluginNames = this.getListOfSupportedPaymentMethods()
+    const slashpayFile = this.generateSlashpayContent(paymentPluginNames)
+    const url = await this.storage.create('./slashpay.json', slashpayFile)
+
+    await this.pluginManager.dispatchEvent('receivePayment', {
+      // TODO: define payload to make plugins create their own slashpay files
+      notificationCallback: this.notificationCallback
+    })
+
+    // XXX what if some plugins failed to initialize?
+    // we need some kind of a mechanism to track their readiness
+    // this can be done via tracking list of plugins which were included into
+    // slashpay.json file and return list as a result of this method
+    // then each plugin should report its readiness via RPC notification endpoint
+    // paymentPluginNames.forEach((name) => {
+    //   this.pluginManager.plugins[name].readyToReceivePayments = false
+    // })
+
+    return url
+  }
+
+  /**
+   * Callback which is called by plugin when payment is received
+   * @param {Object} payload - payment object
+   * @returns {Promise<void>}
+   */
+  async handleNewPayment (payload) {
+    // FIXME: add properties specific to incoming payment
+    // add id?
+    const payment = new Payment({
+      ...payload,
+      sendingPriority: [payload.pluginName],
+      foo: 'test incomming'
+    }, this.db)
+    await payment.save()
+
+    await this.notificationCallback(payment)
+  }
+
+  /**
+   * @method generateSlashpayContent
+   * @param {Array<String>} paymentPluginNames - list of payment plugin names
+   * @returns {Object} - content of slashpay.json file
+   */
+  generateSlashpayContent (paymentPluginNames) {
+    const slashpayFile = { paymentEndpoints: {} }
+    paymentPluginNames.forEach((n) => {
+      slashpayFile.paymentEndpoints[n] = path.join('slashpay', n, 'slashpay.json')
+    })
+
+    return slashpayFile
+  }
+
+  /**
+   * @method getListOfSupportedPaymentMethods
+   * @returns {Array<String>} - list of payment plugin names
+   */
+  getListOfSupportedPaymentMethods () {
+    return Object.entries(this.pluginManager.getPlugins(true))
+      .filter(([_name, { manifest }]) => manifest.type === 'payment')
+      .map(([name, _plugin]) => name)
+  }
+}
+
+module.exports = {
+  PaymentReceiver
+}
---
+++ test/payments/PaymentReceiver.test.js
@@ -0,0 +1,116 @@
+const sinon = require('sinon')
+const { test } = require('brittle')
+
+const { DB } = require('../../src/DB')
+const db = new DB()
+;(async () => { await db.init() })()
+
+const { SlashtagsAccessObject } = require('../../src/SlashtagsAccessObject')
+
+const { PluginManager } = require('../../src/plugins/PluginManager')
+
+const { pluginConfig } = require('../fixtures/config.js')
+
+const { PaymentReceiver } = require('../../src/payments/PaymentReceiver')
+
+test('PaymentReceiver', async t => {
+  const storage = new SlashtagsAccessObject('key', './dir')
+  await storage.init()
+
+  const pluginManager = new PluginManager(pluginConfig)
+  await pluginManager.loadPlugin(pluginConfig.plugins.p2sh, storage)
+
+  const paymentReceiver = new PaymentReceiver(db, pluginManager, storage, () => {})
+
+  t.alike(paymentReceiver.db, db)
+  t.alike(paymentReceiver.storage, storage)
+  t.alike(paymentReceiver.notificationCallback.toString(), '() => {}')
+})
+
+test('PaymentReceiver.init', async t => {
+  const storage = new SlashtagsAccessObject('key', './dir')
+  await storage.init()
+  const storageCreate = sinon.replace(storage, 'create', sinon.fake(storage.create))
+
+  const pluginManager = new PluginManager()
+  await pluginManager.loadPlugin(pluginConfig.plugins.p2sh, storage)
+
+  const pluginDispatch = sinon.replace(pluginManager, 'dispatchEvent', sinon.fake(pluginManager.dispatchEvent))
+
+  const paymentReceiver = new PaymentReceiver(db, pluginManager, storage, () => {})
+
+  const res = await paymentReceiver.init()
+
+  t.ok(res)
+  t.is(storageCreate.callCount, 1)
+  t.is(pluginDispatch.callCount, 1)
+})
+
+test('PaymentReceiver.handleNewPayment', async t => {
+  const storage = new SlashtagsAccessObject('key', './dir')
+  await storage.init()
+
+  const pluginManager = new PluginManager()
+  await pluginManager.loadPlugin(pluginConfig.plugins.p2sh, storage)
+
+  const paymentReceiver = new PaymentReceiver(db, pluginManager, storage, () => {})
+
+  await paymentReceiver.handleNewPayment({
+    pluginName: 'p2sh',
+    orderId: 'testOrderId',
+    clientOrderId: 'testClientOrderId',
+    amount: '1000',
+    targetURL: 'sourgURL'
+  })
+
+  const payment = await db.get('totally-random-id')
+  t.alike(payment, {
+    id: 'totally-random-id',
+    orderId: 'testOrderId',
+    clientOrderId: 'testClientOrderId',
+    internalState: 'initial',
+    targetURL: 'sourgURL',
+    memo: '',
+    amount: '1000',
+    currency: 'BTC',
+    denomination: 'BASE',
+    sendingPriority: ['p2sh'],
+    pendingPlugins: [],
+    triedPlugins: [],
+    currentPlugin: {},
+    sentByPlugin: {},
+    createdAt: payment.createdAt,
+    executeAt: payment.executeAt
+  })
+})
+
+test('PaymentReceiver.generateSlashpayContent', async t => {
+  const storage = new SlashtagsAccessObject('key', './dir')
+  await storage.init()
+
+  const pluginManager = new PluginManager(pluginConfig)
+  await pluginManager.loadPlugin(pluginConfig.plugins.p2sh, storage)
+
+  const paymentReceiver = new PaymentReceiver(pluginManager, db, storage, () => {})
+
+  const slashpayContent = paymentReceiver.generateSlashpayContent(['p2sh', 'p2tr'])
+  t.alike(slashpayContent, {
+    paymentEndpoints: {
+      p2sh: 'slashpay/p2sh/slashpay.json',
+      p2tr: 'slashpay/p2tr/slashpay.json'
+    }
+  })
+})
+
+test('PaymentReceiver.getListOfSupportedPaymentMethods', async t => {
+  const storage = new SlashtagsAccessObject('key', './dir')
+  await storage.init()
+
+  const pluginManager = new PluginManager(pluginConfig)
+  await pluginManager.loadPlugin(pluginConfig.plugins.p2sh, storage)
+
+  const paymentReceiver = new PaymentReceiver(db, pluginManager, storage, () => {})
+
+  const supportedPaymentMethods = paymentReceiver.getListOfSupportedPaymentMethods()
+  t.alike(supportedPaymentMethods, ['p2sh'])
+})
