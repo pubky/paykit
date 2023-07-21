c218bd3fc3f67fdcb56edb34ea3035c82c0f76aa
dzdidi <deniszalessky@gmail.com>
2023-07-21 20:29:46 +0200

    Readme, example
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ examples/README.md
@@ -0,0 +1,31 @@
+# Basic slashpay example:
+
+For the first counterparty run
+`node index.js <path to config>`
+
+
+In separate session run with different config file, ideally connected to different lnd node.
+
+Both instances will be ready to receive payments at the url they print after startup:
+```
+slash:********/public/slashpay.json
+```
+
+To make a payment paste counterparty's url in a prompt and add space separated amount in statoshis.
+
+Watch for notification in both stdouts. One for successfully processing of payment order, and another for successful incoming payment starting with 
+``
+--- notification
+```
+
+## Config file example
+
+```javasript
+module.exports = {
+  CERT: './tls.cert',
+  MACAROON: './admin.macaroon',
+  SOCKET: '127.0.0.1:10007',
+  SUPPORTED_METHODS: ['bolt11', 'p2wpkh'],
+  URL_PREFIX: 'slashpay:'
+}
+```
---
+++ examples/index.js
@@ -0,0 +1,72 @@
+const readline = require('readline')
+
+const { test } = require('brittle')
+const createTestnet = require('@hyperswarm/testnet')
+
+const { SlashtagsConnector, SLASHPAY_PATH } = require('../src/slashtags')
+const { PaymentManager } = require('../src/payments/PaymentManager')
+const { DB } = require('../src/DB') // mocked
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const bolt11 = require('../plugins/btc-l1-l2-lnd/bolt11.js')
+const onchain = require('../plugins/btc-l1-l2-lnd/onchain.js')
+
+;(async () => {
+  const pluginConfig = require(process.argv[2])
+  const slashpayConfig = {
+    sendingPriority: [ 'bolt11', 'onchain' ],
+    plugins: {
+      bolt11,
+      onchain
+    },
+    bolt11: pluginConfig,
+    onchain: pluginConfig
+  }
+
+  const slashtagsConnector = new SlashtagsConnector()
+  await slashtagsConnector.init()
+  const db = new DB()
+  await db.init()
+
+  const paymentManager = new PaymentManager(
+    slashpayConfig,
+    db,
+    slashtagsConnector,
+    (p) => console.log('--- nofication: ', p)
+  )
+
+  await paymentManager.init() // receiver
+  const myUrl = await paymentManager.receivePayments()
+
+  console.log('ready to receive payments at:', myUrl)
+
+  console.log('to send payments paste <slashpayURL> <amount>')
+
+  rl.on('line', async (line) => {
+
+    const [url, amount] = line.split(' ')
+    const paymentOrder = await paymentManager.createPaymentOrder({
+      clientOrderId: Date.now(),
+      amount,
+      sendingPriority: [
+        'bolt11',
+        'onchain',
+      ],
+      counterpartyURL: url
+    })
+
+    await paymentManager.sendPayment(paymentOrder.id)
+  })
+
+  rl.on('close', async () => {
+    console.log('bye')
+    await slashtagsConnector.close()
+    // XXX: stop plugins shutting down the subscriptions to addresses and invoices
+    process.exit(0)
+  })
+
+})()
